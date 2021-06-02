56bf156938c743740751f15d3ce5a1e693494617
annechinn <achinn@nwlink.com>
2021-06-02 09:58:57 -0700

    Delete tech.js

--- scripts/tech.js
+++
@@ -1,7 +0,0 @@
-function onTechLinkClicked(event) {
-    // prevents page from refreshing when the anchor is clicked
-    event.preventDefault();
-    
-    let targetElement = event.target;
-    targetElement.classList.add("active");
-    }
